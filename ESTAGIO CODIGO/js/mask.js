3af52bd02ada8e94c821cafc56ab895dc9b12eb8
JoaoAugustto <joaoaugusttoxd@gmail.com>
2023-11-08 22:29:18 -0300

    V5

---
+++ ESTAGIO CODIGO/js/mask.js
@@ -0,0 +1,7 @@
+$(".mask_phone").mask("(00) 0 0000-0000");
+$(".mask_telephone").mask("(00) 0000-0000");
+$('.mask_value').mask("#.##0,00", {reverse: true});
+$(".mask_cep").mask("00000-000");
+$(".mask_cnpj").mask("00.000.000/0000-00");
+$(".mask_cpf").mask("000.000.000-00");
+$(".mask_rg").mask("00.000.000-00");
